190433eaceaa50f3cefb8b2880b81a2e5c3d2098
Alexey Karapetov <karapetov@gmail.com>
2018-09-14 15:59:35 -0700

    Nming rules

--- lib/src/strict_naming.dart
+++ lib/src/strict_naming.dart
@@ -1,12 +1,14 @@
 import 'naming.dart';
 
-/// Strict naming standards
+/// Strict naming rules
 class StrictNaming extends Naming {
   static final _disallowFirst = new RegExp(r'^[^_-]');
   static final _disallowLast = new RegExp(r'[^_-]$');
   static final _allowGlobally = new RegExp(r'^[a-zA-Z0-9_-]+$');
+  static const instance = const StrictNaming.constInstance();
 
-  const StrictNaming();
+  const StrictNaming.constInstance();
+  factory StrictNaming() => instance;
 
   bool allows(String name) =>
       _disallowFirst.hasMatch(name) &&
