908783e3270cb2b16823503f751c9989c5e8d2a0
Alexey Karapetov <karapetov@gmail.com>
2018-10-09 11:57:10 -0700

    LinkObject

--- lib/src/link.dart
+++ lib/src/link.dart
@@ -5,7 +5,7 @@ import 'package:json_api_document/src/meta.dart';
 /// http://jsonapi.org/format/#document-links
 class Link {
   final String url;
-  final isObject = false;
+  final bool isObject = false;
   final Meta meta = null;
 
   Link(String this.url);
@@ -13,4 +13,22 @@ class Link {
   toJson() => url;
 
   static Link fromJson(json) => Link(json);
+}
+
+/// A Link object.
+///
+/// http://jsonapi.org/format/#document-links
+class LinkObject implements Link {
+  final String url;
+  final bool isObject = true;
+  final Meta meta;
+
+  LinkObject(String this.url, {Map<String, dynamic> meta})
+      : meta = Meta.fromJson(meta);
+
+  toJson() {
+    final Map<String, dynamic> json = {'href': url};
+    if (meta != null) json['meta'] = meta;
+    return json;
+  }
 }
