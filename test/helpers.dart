2f7d509957a7a4ec7f36b1458446f7c95ade680b
Alexey Karapetov <karapetov@gmail.com>
2019-01-08 19:46:01 -0800

    Wording

--- test/helpers.dart
+++ test/helpers.dart
@@ -2,5 +2,5 @@ import 'dart:convert';
 
 /// Strips types from a json object.
 /// This way we can simulate real world use cases when JSON comes in a string
-/// rather than a Dart object.
+/// rather than a Dart object, thus not having all type information.
 recodeJson(j) => json.decode(json.encode(j));
