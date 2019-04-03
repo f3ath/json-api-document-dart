d5ed53f441518dbeba89ab9ef6764e6114febd59
Alexey <f3ath@users.noreply.github.com>
2019-04-03 10:56:39 -0700

    Update resource_object_test.dart

--- test/resource_object_test.dart
+++ test/resource_object_test.dart
@@ -4,7 +4,7 @@ import 'package:test/test.dart';
 
 void main() {
   group('ResourceObject', () {
-    /// id:null should not present in JSON
+    /// id:null should not be included in JSON
     /// https://jsonapi.org/format/#crud-creating
     test('id:null should not be included in JSON', () {
       final res = ResourceObject('photos', null, attributes: {
